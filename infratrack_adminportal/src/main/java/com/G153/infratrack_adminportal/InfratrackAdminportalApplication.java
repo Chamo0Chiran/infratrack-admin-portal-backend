6a4d48ece46c63c5a037dbf93d06b298084e32b6
Chamodh <chamochian05@gmail.com>
2025-02-03 22:19:59 +0530

    first commit

---
+++ infratrack_adminportal/src/main/java/com/G153/infratrack_adminportal/InfratrackAdminportalApplication.java
@@ -0,0 +1,13 @@
+package com.G153.infratrack_adminportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfratrackAdminportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfratrackAdminportalApplication.class, args);
+	}
+
+}
---
+++ infratrack_adminportal/src/test/java/com/G153/infratrack_adminportal/InfratrackAdminportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G153.infratrack_adminportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfratrackAdminportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
